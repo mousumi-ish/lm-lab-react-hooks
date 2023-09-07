c42e7848f06016eb9d47c52ffa8dd9a366d2e427
mousumi-ish <baral_mousumi@yahoo.com>
2023-09-07 14:19:54 +0100

    feat: refactor code for use_effect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,43 @@
 // make an api call using useEffect
+import { useState, useEffect } from "react";
 
-export const APICall = () => {
-
-    // Your code here!
-
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
+const fetchTodo = async () => {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/2"
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
+export const APICall = () => {
+  // Your code here!
+  const [todoList, setJson] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const todoData = await fetchTodo();
+      setJson(todoData);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      {todoList ? <p>{todoList.title}</p> : <p>please wait...</p>}
+      {todoList === null && <p>Error fetching data.</p>}
+    </>
+  );
+};
